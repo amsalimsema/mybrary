69621d335cb295b57fee1bd0dae5a8ba430da606
Salim Semakula <salimsema@yahoo.com>
2021-10-29 16:08:44 +0300

    InitialSetUp

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express()
+const expressLayouts = require('express-ejs-layouts')
+
+const indexRouter = require('./routes/index')
+
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')
+app.set('layout', 'layouts/layout')
+app.use(expressLayouts)
+app.use(express.static('public'))
+
+const dotenv = require('dotenv')
+dotenv.config()
+const mongoose = require('mongoose')
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    // useCreateIndex: true,
+    // useFindAndModify: true,
+  })
+  .then(console.log('Connected to MongoDB'))
+  .catch((err) => console.log(err))
+
+app.use('/', indexRouter)
+
+app.listen(process.env.PORT || 5000)
